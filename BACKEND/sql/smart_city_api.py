d6401525efdbd77489dd8991706d91167ce2951b
2402212-zixin <2402212@sit.singaporetech.edu.sg>
2025-11-27 17:20:32 +0800

    update frontend

--- BACKEND/sql/smart_city_api.py
+++ BACKEND/sql/smart_city_api.py
@@ -17,7 +17,7 @@ import requests
 DB_CONFIG = {
     'host': 'localhost',
     'user': 'root',
-    'password': 'password',  # Update with your DB password
+    'password': '',  # Update with your DB password
     'database': 'smart_city'
 }
 
